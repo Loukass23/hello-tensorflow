f3a395f4782b780a0cb8570215e9abd1bef21248
loukass23 <l.dupias@net.estia.fr>
2020-01-09 18:40:51 +0100

    gpu run try

--- text_classification.py
+++ text_classification.py
@@ -1,11 +1,20 @@
+# import tensorflow as tf
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import tensorflow as tf
-from tensorflow import keras 
+from tensorflow import keras
+
+
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+
 import numpy as np
 
+print(tf.config.list_physical_devices('GPU'))
 
 data = keras.datasets.imdb
 
-(train_data, train_laels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 # print(train_data)
 
@@ -35,7 +44,55 @@ def decode_review(text):
 
 # define Model and layers
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.Embedding(88000, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense( 16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
+
+model.summary()
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+ 
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+ 
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+# print(results)
+
+# model.save("model.h5")
+
+def review_encode(s):
+    encode = [1]
+
+    for word in s:
+        if word in words_index:
+            encode.append(words_index[word.lower()])
+        else:
+            encode.append(2)
+    return encode
+
+# model = keras.models.load_model("model.h5")
+
+with open("LOTR.txt") as f:
+    for line in f.readlines():
+        nline = line.replace(",", "").replace(".", "").replace("()", "").replace(":", "").replace("\"", "").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=words_index["<PAD>"], padding="post", maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
+
+
+# testmodel
+# test_review = test_data[0]
+# predict = model.predict([test_review])
+# print("Review: ")
+# print(decode_review(test_review))
+# print("Prediction:" + str(predict[0]))
+# print("Actual:" + str(test_labels[0]))
+# print(results)
